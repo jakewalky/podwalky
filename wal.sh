a057a1768b01665732c004d626f46655db9841bb
jakewalky <94923185+jakewalky@users.noreply.github.com>
2022-09-24 13:01:56 +0000

    Create wal.sh

---
+++ wal.sh
@@ -0,0 +1,18 @@
+apt-get update
+echo "Download windows files"
+wget -O w7x64.img https://bit.ly/akuhnetw7X64
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+./ngrok authtoken 1tHymSxzUttJMRnvGZbzvEfPvgP_712gXFjD4qVYZEzujEv9m
+nohup ./ngrok tcp 3388 &>/dev/null &
+echo Downloading File From akuh.net
+apt-get install qemu-system-x86 -y
+echo "Wait"
+echo "Starting Windows"
+qemu-system-x86_64 -hda w7x64.img -m 4G -smp cores=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+clear
+echo RDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+sleep 432000
